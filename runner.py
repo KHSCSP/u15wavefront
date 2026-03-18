22481bae5d42c4bb22fb7ee4f4d3a280125c5d30
scneeman <30477959+scneeman@users.noreply.github.com>
2026-03-18 09:52:36 -0500

    Create runner.py

---
+++ runner.py
@@ -0,0 +1,42 @@
+# backlog:
+# main algorithm
+# show()
+# do_wave()
+# check_nesw(row, col)
+
+
+# --- the variables ---------------------------
+# beginning is [0][0], marked with 's'
+# end is [5][5], marked with 1
+maze = [
+        ['s', 0, 0, 'x', 0, 0],
+        ['x', 0, 'x', 0, 0, 0],
+        ['x', 0, 'x', 0, 'x', 0],
+        [0, 0, 'x', 0, 'x', 0],
+        [0, 0, 'x', 0, 'x', 0],
+        [0, 0, 0, 0, 'x', 1]
+    ]
+
+        
+
+
+
+# --- the functions -------------------------------
+def show():
+    pass
+
+
+def check_nesw(row, col):
+    pass
+    
+
+
+def do_wave():
+    pass
+                
+
+# --- the main runner -------------------------------               
+
+
+
+    
